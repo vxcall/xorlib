a55ba18ebc0330f45427ba343bd87b58f780485d
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-03-07 03:39:09 +0800

    Implemented a base encryption code.
    I coded a chunk of code which calculates and turns each passed characters into encrypted form.
    Not any exclusive code, just a base of the idea.

--- include/xorlib.h
+++ include/xorlib.h
@@ -3,10 +3,16 @@
 #define XORLIB_INLINE inline
 
 #include <iostream>
+#include <string>
 
+char key = 'd';
 namespace xorlib {
-    XORLIB_INLINE void encrypt() {
-        std::cout << "xorlib initial commit!" << std::endl;
-        return;
+    XORLIB_INLINE char* encrypt(const char* plaintext) {
+        char* result = new char[strlen(plaintext)+1];
+        for (unsigned short i = 0;i <= strlen(plaintext);++i) {
+            char cipher = plaintext[i] ^ key;
+            result[i] = cipher;
+        }
+        return result;
     }
 }
