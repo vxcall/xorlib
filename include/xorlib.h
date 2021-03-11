a781b33a4c49c1bb7a1d8985787e39b6ae9c0153
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-03-11 16:20:01 +0800

    Implemented a simple encryption!
    I've finished implementing a prototype meaning a simple encryption header library which does its job. But still can be much greater.

--- include/xorlib.h
+++ include/xorlib.h
@@ -1,21 +1,35 @@
 #pragma once
-
-#define XORLIB_INLINE inline
-
 #include <iostream>
-#include <string>
+#include <array>
 
-char key = 'd';
-namespace xorlib {
-    XORLIB_INLINE std::string encrypt(const char* plaintext) {
-        char* buffer = new char[strlen(plaintext)+1];
-        for (unsigned short i = 0;i <= strlen(plaintext);++i) {
-            char cipher = plaintext[i] ^ key;
-            buffer[i] = cipher;
-        }
-        buffer += '\0';
-        std::string result(buffer);
-        delete [] buffer;
-        return result;
+// Every functions must be inlined to avoid ODR violation
+#define XORLIB_FORCEINLINE __forceinline
+
+template<size_t N>
+class Xorlib {
+    char key = 'k';
+    [[nodiscard]] constexpr char xoring(char c) const
+    {
+        return c ^ key;
     }
-}
+public:
+    // Can't be a std::vector because std::vector allocates memory at runtime.
+    // Also can't be a char[N+1] because there's no good way to return char array from decrypt() function.
+    std::array<char, N+1> encrypted;
+
+    template<size_t... Seq>
+    XORLIB_FORCEINLINE constexpr Xorlib(const char* str, std::index_sequence<Seq...>) : encrypted{ xoring(str[Seq])...}{
+        encrypted[N] = '\0';
+    };
+
+    XORLIB_FORCEINLINE char* decrypt()
+    {
+        for (size_t i = 0; i < N; ++i) {
+            encrypted[i] = xoring(encrypted[i]);
+        }
+        return encrypted.data();
+    }
+};
+
+// A neat macro to use this easily
+#define _(s) []{constexpr Xorlib<sizeof(s) - 1> xorlib(s, std::make_index_sequence<sizeof(s) - 1>()); return xorlib;}().decrypt()
